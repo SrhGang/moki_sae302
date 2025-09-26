d191ffe277a506e0dcc93eaca4c917448465b5d6
SrhGang <srhgang5@gmail.com>
2025-09-26 09:04:39 +0400

    fix: correction des bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 import NotFound from './pages/NotFound';
-import Avatar from './pages/Avatar';
-import Dashboard from './pages/Dashboard';
+
+
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import Avatar from './pages/Avatar';
+// import Dashboard from './pages/Dashboard';
+
+
 import './styles/global.css';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+
+        {/*
+        
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/avatar" element={<Avatar />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} /> 
+        
+        */}
       </Routes>
     </Router>
   );
